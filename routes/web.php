8751675843d2c40de9b89c559364fe795ca2a2cb
apw516 <redhawk5665@gmail.com>
2024-01-09 20:00:11 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\PendaftaranController;
 use App\Http\Controllers\FarmasiController;
 use App\Http\Controllers\DokterController;
 use App\Http\Controllers\LaboratoriumController;
-use App\Http\Controllers\LandingController;
 use App\Http\Controllers\PerawatController;
 
 
@@ -19,12 +18,19 @@ Route::get('/', [DashboardController::class, 'index'])->middleware('guest')->nam
 Route::get('DaftarPelayanan', [PendaftaranController::class, 'DaftarPelayanan'])->middleware('guest')->name('DaftarPelayanan');
 
 
+
+
 //farmasi route
 Route::get('Layananresep', [FarmasiController::class, 'Layananresep'])->middleware('guest')->name('Layananresep');
+
+
+
 
 //RME DOKTER
 Route::get('rmedokter', [DokterController::class, 'index'])->middleware('guest')->name('rmedokter');
 Route::post('assesmendokter', [DokterController::class, 'assesmendokter'])->middleware('guest')->name('assesmendokter');
+
+
 
 //RME PERAWAT
 Route::get('rmeperawat', [PerawatController::class, 'index'])->middleware('guest')->name('rmeperawat');
