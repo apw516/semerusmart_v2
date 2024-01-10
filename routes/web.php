e4f4e6bb05989140ec071c4b8d7fe39a14d34355
apw516 <redhawk5665@gmail.com>
2024-01-10 22:25:40 +0700

    update web

--- routes/web.php
+++ routes/web.php
@@ -1,208 +1,110 @@
-<!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-teal elevation-4">
-    <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link bg-teal">
-        <img src="{{ asset('public/adminlte/dist/img/LOGOX.svg') }}" alt="AdminLTE Logo"
-            class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">SEMERUSMART</span>
-    </a>
-    <!-- Sidebar -->
-    <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="{{ asset('public/adminlte/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
-                    alt="User Image">
-            </div>
-            <div class="info">
-                <a href="#" class="d-block">Admin</a>
-            </div>
-        </div>
+<?php
 
-        <!-- Sidebar Menu -->
-        <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                data-accordion="false">
-                <li class="nav-item">
-                    <a href="{{ route('dashboard') }}" class="nav-link @if($menu == 'Dashboard' ) active @endif">
-                        <i class="nav-icon fas fa-th"></i>
-                        <p>
-                            Dashboard
-                            {{-- <span class="right badge badge-danger">New</span> --}}
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-header">PENDAFTARAN</li>
-                <li class="nav-item">
-                    <a href="{{ route('DaftarPelayanan')}}" class="nav-link @if($menu == 'Daftar Pelayanan' ) active @endif">
-                        <i class="nav-icon fas fa-archive"></i>
-                        <p>
-                            Daftar Pelayanan
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('RiwayatPendaftaran')}}" class="nav-link @if($menu == 'Riwayat Pendaftaran' ) active @endif">
-                        <i class="nav-icon fas fa-archive"></i>
-                        <p>
-                            Riwayat Pendaftaran
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-header">ERM</li>
-                <li class="nav-item">
-                    <a href="{{ route('rmedokter')}}" class="nav-link @if($menu == 'RME DOKTER' ) active @endif">
-                        <i class="nav-icon fas fa-archive"></i>
-                        <p>
-                            RME DOKTER
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('rmeperawat')}}" class="nav-link @if($menu == 'RME PERAWAT' ) active @endif">
-                        <i class="nav-icon fas fa-archive"></i>
-                        <p>
-                            RME PERAWAT
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-header">FARMASI</li>
-                <li class="nav-item">
-                    <a href="{{ route('Layananresep') }}" class="nav-link @if($menu == 'Layanan Resep' ) active @endif">
-                        <i class="nav-icon fas fa-archive"></i>
-                        <p>
-                            Layanan Resep
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="" class="nav-link @if($menu == 'Riwayat Pendaftaran1' ) active @endif">
-                        <i class="nav-icon fas fa-archive"></i>
-                        <p>
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PendaftaranController;
+use App\Http\Controllers\FarmasiController;
+use App\Http\Controllers\DokterController;
+use App\Http\Controllers\LaboratoriumController;
+use App\Http\Controllers\PerawatController;
+use App\Http\Controllers\LandingController;
+use App\Http\Controllers\KasirController;
 
-                            Riwayat Resep
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="" class="nav-link @if($menu == 'Riwayat Pendaftaran1' ) active @endif">
-                        <i class="nav-icon fas fa-archive"></i>
-                        <p>
-                            Kartu Stok Obat
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-header">GUDANG FARMASI</li>
-                <li class="nav-item">
-                    <a href="{{ route('Layananresep') }}" class="nav-link @if($menu == 'Stok Barang' ) active @endif">
-                        <i class="nav-icon fas fa-archive"></i>
-                        <p>
-                            Stok Barang
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('indexmastersupplier')}}" class="nav-link @if($menu == 'Master Supplier' ) active @endif">
-                        <i class="nav-icon fas fa-archive"></i>
-                        <p>
-                            Master Supplier
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('masterbarang')}}" class="nav-link @if($menu == 'Master Barang' ) active @endif">
-                        <i class="nav-icon fas fa-archive"></i>
-                        <p>
-                            Master Barang
+//dashboard route
+Route::get('/', [LandingController::class, 'index'])->middleware('guest')->name('landing');
+Route::get('dashboardlanding', [LandingController::class, 'Dashboard_landing'])->middleware('guest')->name('dashboardlanding');
+Route::get('jadwalpoli', [LandingController::class, 'Jadwal_poli'])->middleware('guest')->name('jadwalpoli');
+Route::get('kontakkami', [LandingController::class, 'Kontak_kami'])->middleware('guest')->name('kontakkami');
+Route::get('login', [LandingController::class, 'login'])->middleware('guest')->name('login');
 
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-header">PENUNJANG</li>
-                <li class="nav-item">
-                    <a href="{{ route('laboratorium') }}" class="nav-link @if($menu == 'Laboririum' ) active @endif">
-                        <i class="nav-icon fas fa-archive"></i>
-                        <p>
-                           LABORATORIUM
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="" class="nav-link @if($menu == 'Riwayat Pendaftaran1' ) active @endif">
-                        <i class="nav-icon fas fa-archive"></i>
-                        <p>
-                            Menu 2
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-header">KASIR</li>
-                <li class="nav-item">
-                    <a href="{{ route('kasir') }}" class="text-tile nav-link @if($menu == 'Kasir' ) active @endif">
-                        <i class="nav-icon fas fa-money-bill"></i>
-                        <p>
-                            Pembayaran
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="" class="nav-link @if($menu == 'Riwayat Pendaftaran1' ) active @endif">
-                        <i class="nav-icon fas fa-archive"></i>
-                        <p>
-                            Menu 2
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-header">REPORTING</li>
-                <li class="nav-item">
-                    <a href="" class="nav-link @if($menu == 'Pendaftaran1' ) active @endif">
-                        <i class="nav-icon fas fa-archive"></i>
-                        <p>
-                            Menu 1
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="" class="nav-link @if($menu == 'Riwayat Pendaftaran1' ) active @endif">
-                        <i class="nav-icon fas fa-archive"></i>
-                        <p>
-                            Menu 2
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-header">DATA MASTER</li>
-                <li class="nav-item">
-                    <a href="" class="nav-link @if($menu == 'Pendaftaran1' ) active @endif">
-                        <i class="nav-icon fas fa-archive"></i>
-                        <p>
-                            Menu 1
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="" class="nav-link @if($menu == 'Riwayat Pendaftaran1' ) active @endif">
-                        <i class="nav-icon fas fa-archive"></i>
-                        <p>
-                            Menu 2
-                        </p>
-                    </a>
-                </li>
+Route::get('dashboard', [DashboardController::class, 'index'])->middleware('guest')->name('dashboard');
+Route::get('/', [DashboardController::class, 'index'])->middleware('guest')->name('dashboard');
+Route::get('dashboard', [DashboardController::class, 'index'])->middleware('guest')->name('dashboard');
 
-                <li class="nav-header">Akun</li>
-                <li class="nav-item">
-                    <a href="" class="nav-link @if($menu == 'Profil' ) active @endif">
-                        <i class="nav-icon far fa-circle text-danger"></i>
-                        <p class="text">Info Akun</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" onclick="logout()">
-                        <i class="nav-icon far fa-circle text-warning"></i>
-                        <p>Logout</p>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-</aside>
+
+//pendaftaran route
+Route::get('DaftarPelayanan', [PendaftaranController::class, 'DaftarPelayanan'])->middleware('guest')->name('DaftarPelayanan');
+Route::get('RiwayatPendaftaran', [PendaftaranController::class, 'Riwayat_pendaftaran'])->middleware('guest')->name('RiwayatPendaftaran');
+
+Route::get('dashboard', [DashboardController::class, 'index'])->middleware('guest')->name('dashboard');
+Route::get('dashboard', [DashboardController::class, 'index'])->middleware('guest')->name('dashboard');
+Route::get('/', [DashboardController::class, 'index'])->middleware('guest')->name('dashboard');
+Route::get('dashboard', [DashboardController::class, 'index'])->middleware('guest')->name('dashboard');
+Route::get('Kasir', [KasirController::class, 'index'])->middleware('guest')->name('kasir');
+//pendaftaran route
+Route::get('DaftarPelayanan', [PendaftaranController::class, 'DaftarPelayanan'])->middleware('guest')->name('DaftarPelayanan');
+Route::get('RiwayatPendaftaran', [PendaftaranController::class, 'Riwayat_pendaftaran'])->middleware('guest')->name('RiwayatPendaftaran');
+Route::get('cariprovinsi', [PendaftaranController::class, 'Cari_provinsi'])->middleware('guest')->name('cariprovinsi');
+Route::get('carikabupaten', [PendaftaranController::class, 'Cari_kabupaten'])->middleware('guest')->name('carikabupaten');
+Route::get('carikecamatan', [PendaftaranController::class, 'Cari_kecamatan'])->middleware('guest')->name('carikecamatan');
+Route::get('caridesa', [PendaftaranController::class, 'Cari_desa'])->middleware('guest')->name('caridesa');
+Route::post('ambildatapasien', [PendaftaranController::class, 'Ambil_data_pasien'])->name('ambildatapasien');
+
+Route::post('ambil_riwayat_pendaftaran', [PendaftaranController::class, 'Ambil_riwayat_pendaftaran'])->name('ambil_riwayat_pendaftaran');
+Route::post('caripasien', [PendaftaranController::class, 'Cari_pasien'])->name('caripasien');
+Route::post('ambildetailpasien', [PendaftaranController::class, 'Ambil_detail_pasien'])->name('ambildetailpasien');
+Route::post('simpanpendaftaran', [PendaftaranController::class, 'Simpan_pendaftaran'])->name('simpanpendaftaran');
+
+//Farmasi Route
+Route::get('masterbarang', [FarmasiController::class, 'Master_barang'])->middleware('guest')->name('masterbarang');
+Route::post('ambil_master_barang', [FarmasiController::class, 'Ambil_master_barang'])->middleware('guest')->name('ambil_master_barang');
+
+Route::post('caripasien', [PendaftaranController::class, 'Cari_pasien'])->name('caripasien');
+Route::post('ambildetailpasien', [PendaftaranController::class, 'Ambil_detail_pasien'])->name('ambildetailpasien');
+Route::post('simpanpendaftaran', [PendaftaranController::class, 'Simpan_pendaftaran'])->name('simpanpendaftaran');
+
+Route::get('RiwayatPendaftaran', [PendaftaranController::class, 'Riwayat_pendaftaran'])->middleware('guest')->name('RiwayatPendaftaran');
+
+
+Route::get('cariprovinsi', [PendaftaranController::class, 'Cari_provinsi'])->middleware('guest')->name('cariprovinsi');
+Route::get('carikabupaten', [PendaftaranController::class, 'Cari_kabupaten'])->middleware('guest')->name('carikabupaten');
+Route::get('carikecamatan', [PendaftaranController::class, 'Cari_kecamatan'])->middleware('guest')->name('carikecamatan');
+Route::get('caridesa', [PendaftaranController::class, 'Cari_desa'])->middleware('guest')->name('caridesa');
+Route::post('ambildatapasien', [PendaftaranController::class, 'Ambil_data_pasien'])->name('ambildatapasien');
+Route::post('ambil_riwayat_pendaftaran', [PendaftaranController::class, 'Ambil_riwayat_pendaftaran'])->name('ambil_riwayat_pendaftaran');
+Route::post('caripasien', [PendaftaranController::class, 'Cari_pasien'])->name('caripasien');
+Route::post('ambildetailpasien', [PendaftaranController::class, 'Ambil_detail_pasien'])->name('ambildetailpasien');
+Route::post('simpanpendaftaran', [PendaftaranController::class, 'Simpan_pendaftaran'])->name('simpanpendaftaran');
+
+Route::post('simpanpasienbaru', [PendaftaranController::class, 'Simpan_pasien_baru'])->name('simpanpasienbaru');
+
+//Farmasi Route
+Route::get('masterbarang', [FarmasiController::class, 'Master_barang'])->middleware('guest')->name('masterbarang');
+Route::get('indexmastersupplier', [FarmasiController::class, 'Index_master_supplier'])->middleware('guest')->name('indexmastersupplier');
+Route::post('ambil_master_barang', [FarmasiController::class, 'Ambil_master_barang'])->middleware('guest')->name('ambil_master_barang');
+Route::post('ambil_master_supplier', [FarmasiController::class, 'Ambil_master_supplier'])->middleware('guest')->name('ambil_master_supplier');
+Route::post('simpanmasterbarang', [FarmasiController::class, 'Simpan_master_barang'])->middleware('guest')->name('simpanmasterbarang');
+Route::post('simpanmastersupplier', [FarmasiController::class, 'Simpan_master_supplier'])->middleware('guest')->name('simpanmastersupplier');
+
+
+Route::post('ambil_riwayat_pendaftaran', [PendaftaranController::class, 'Ambil_riwayat_pendaftaran'])->name('ambil_riwayat_pendaftaran');
+Route::post('caripasien', [PendaftaranController::class, 'Cari_pasien'])->name('caripasien');
+Route::post('ambildetailpasien', [PendaftaranController::class, 'Ambil_detail_pasien'])->name('ambildetailpasien');
+Route::post('simpanpendaftaran', [PendaftaranController::class, 'Simpan_pendaftaran'])->name('simpanpendaftaran');
+
+//Farmasi Route
+Route::get('masterbarang', [FarmasiController::class, 'Master_barang'])->middleware('guest')->name('masterbarang');
+Route::post('ambil_master_barang', [FarmasiController::class, 'Ambil_master_barang'])->middleware('guest')->name('ambil_master_barang');
+Route::post('caripasien', [PendaftaranController::class, 'Cari_pasien'])->name('caripasien');
+Route::post('ambildetailpasien', [PendaftaranController::class, 'Ambil_detail_pasien'])->name('ambildetailpasien');
+Route::post('simpanpendaftaran', [PendaftaranController::class, 'Simpan_pendaftaran'])->name('simpanpendaftaran');
+
+
+//farmasi route
+Route::get('Layananresep', [FarmasiController::class, 'Layananresep'])->middleware('guest')->name('Layananresep');
+
+
+
+//RME DOKTER
+Route::get('rmedokter', [DokterController::class, 'index'])->middleware('guest')->name('rmedokter');
+Route::post('assesmendokter', [DokterController::class, 'assesmendokter'])->middleware('guest')->name('assesmendokter');
+
+
+
+//RME PERAWAT
+Route::get('rmeperawat', [PerawatController::class, 'index'])->middleware('guest')->name('rmeperawat');
+Route::post('assesmenperawat', [PerawatController::class, 'assesmenperawat'])->middleware('guest')->name('assesmenperawat');
+
+//penunjang
+Route::get('laboratorium', [LaboratoriumController::class, 'index'])->middleware('guest')->name('laboratorium');
+Route::post('detailorderlab', [LaboratoriumController::class, 'detailorderlab'])->middleware('guest')->name('detailorderlab');
